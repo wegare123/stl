4c01ab61f60d954e87a5381174df5f4cb579c822
wegare123 <56117745+wegare123@users.noreply.github.com>
2021-01-24 07:13:58 +0700

    Add files via upload

--- stl2/install.sh
+++ stl2/install.sh
@@ -4,19 +4,19 @@ printf 'ctrl+c' | crontab -e > /dev/null
 opkg update && opkg install wget
 cek=$(uname -m)
 if [[ $cek == "mips" ]]; then
-wget --no-check-certificate "https://github.com/wegare123/stl-tunnel/blob/main/Gotun/tun2socks-mips?raw=true" -O /usr/bin/tun2socks
-wget --no-check-certificate "https://github.com/wegare123/stl-tunnel/blob/main/Gotun/sshpass_1.06-wegare_mips.ipk?raw=true" -O ~/tun2socks.ipk
+wget --no-check-certificate "https://github.com/wegare123/stl/blob/main/stl2/tun2socks-mips?raw=true" -O /usr/bin/tun2socks
+wget --no-check-certificate "https://github.com/wegare123/stl/blob/main/stl2/sshpass_1.06-wegare_mips.ipk?raw=true" -O ~/tun2socks.ipk
 elif [[ $cek == "x86" ]]; then
-wget --no-check-certificate "https://github.com/wegare123/stl-tunnel/blob/main/Gotun/tun2socks-386?raw=true" -O /usr/bin/tun2socks
-wget --no-check-certificate "https://github.com/wegare123/stl-tunnel/blob/main/Gotun/sshpass_1.06-wegare_i386_pentium.ipk?raw=true" -O ~/tun2socks.ipk
+wget --no-check-certificate "https://github.com/wegare123/stl/blob/main/stl2/tun2socks-386?raw=true" -O /usr/bin/tun2socks
+wget --no-check-certificate "https://github.com/wegare123/stl/blob/main/stl2/sshpass_1.06-wegare_i386_pentium.ipk?raw=true" -O ~/tun2socks.ipk
 else
 echo -e "version anda tidak terdeteksi!"
 exit
 fi
-wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/stl-tunnel/main/Gotun/stl.sh" -O /usr/bin/stl
-wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/stl-tunnel/main/Gotun/gproxy.sh" -O /usr/bin/gproxy
-wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/stl-tunnel/main/Gotun/autorekonek-stl.sh" -O /usr/bin/autorekonek-stl
-opkg install openvpn && opkg install ip-full && opkg install openssh-client && opkg install stunnel && opkg install *.ipk && opkg install lsof && opkg install fping && opkg install bash
+wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/stl/main/stl2/stl.sh" -O /usr/bin/stl
+wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/stl/main/stl2/gproxy.sh" -O /usr/bin/gproxy
+wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/stl/main/stl2/autorekonek-stl.sh" -O /usr/bin/autorekonek-stl
+opkg install openvpn && opkg install ip-full && opkg install openssh-client && opkg install stunnel && opkg install *.ipk && opkg install lsof && opkg install fping && opkg install screen
 chmod +x /usr/bin/gproxy
 chmod +x /usr/bin/stl
 chmod +x /usr/bin/autorekonek-stl
