2f1b8183b5ebfbcf5317defae0f736bf02163a92
wegare123 <56117745+wegare123@users.noreply.github.com>
2021-08-29 20:30:17 +0700

    Add files via upload

--- stl/install.sh
+++ stl/install.sh
@@ -23,17 +23,28 @@ elif [[ $cek == *"OpenWrt"* ]] && [[ $cek == *"aarch64_cortex-a53"* ]]; then
 wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/openwrt/sunxi.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/sunxi/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/sunxi
 elif [[ $cek == *"OpenWrt"* ]] && [[ $cek == *"mips_24kc"* ]]; then
 wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/openwrt/ar71xx.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/ar71xx/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/ar71xx
+elif [[ $cek == *"OpenWrt"* ]] && [[ $cek == *"mipsel_24kc"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/openwrt/ramips.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/ramips/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/ramips
+elif [[ $cek == *"OpenWrt"* ]] && [[ $cek == *"x86"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/openwrt/i386-pentium.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/i386-pentium/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/i386-pentium
 else
 echo -e "version anda tidak terdeteksi!"
 exit
 fi
-wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/stl-tunnel/main/stl.sh" -O /usr/bin/stl
-wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/stl-tunnel/main/gproxy.sh" -O /usr/bin/gproxy
-wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/stl-tunnel/main/autorekonek-stl.sh" -O /usr/bin/autorekonek-stl
-wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/stl-tunnel/main/redsocks.conf" -O /etc/redsocks-stl.conf
-opkg install openvpn-openssl && opkg install ip-full && opkg install openssh-client && opkg install stunnel && opkg install redsocks && opkg install *.ipk && opkg install lsof && opkg install sshpass
+wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/stl/main/stl/stl.sh" -O /usr/bin/stl
+wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/stl/main/stl/gproxy.sh" -O /usr/bin/gproxy
+wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/stl/main/stl/autorekonek-stl.sh" -O /usr/bin/autorekonek-stl
+wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/stl/main/stl/tunnel.py" -O /root/akun/tunnel.py
+wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/stl/main/stl/ssh.py" -O /root/akun/ssh.py
+wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/stl/main/stl/inject.py" -O /root/akun/inject.py
+wget --no-check-certificate "https://github.com/wegare123/stl/blob/main/stl/Brainfuck-master.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && mv ~/Brainfuck-master/* ~/ && mv ~/Brainfuck-master/.brainfuck-tunnel ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/Brainfuck-master
+opkg install openvpn-openssl && opkg install ip-full && opkg install openssh-client && opkg install stunnel && opkg install *.ipk && opkg install lsof && opkg install fping screen python python3
+chmod +x /root/brainfuck
 chmod +x /usr/bin/gproxy
 chmod +x /usr/bin/stl
+chmod +x /root/akun/tunnel.py
+chmod +x /root/akun/ssh.py
+chmod +x /root/akun/inject.py
 chmod +x /usr/bin/autorekonek-stl
 rm -r ~/*.ipk
 rm -r ~/install.sh
@@ -41,6 +52,7 @@ mkdir -p ~/akun/
 mkdir -p ~/.ssh/
 touch ~/akun/ssl.conf
 touch ~/.ssh/config
+touch ~/akun/stl.txt
 sleep 2
 echo "install selesai"
 echo "untuk memulai tools silahkan jalankan perintah 'stl'"
