7ec420d7a7044550bf6c4b3ed2fb313d30e90be0
wegare123 <56117745+wegare123@users.noreply.github.com>
2021-09-05 17:00:42 +0700

    Add files via upload

--- stl/install.sh
+++ stl/install.sh
@@ -40,7 +40,7 @@ wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/stl/mai
 wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/stl/main/stl/tunnel.py" -O /root/akun/tunnel.py
 wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/stl/main/stl/ssh.py" -O /root/akun/ssh.py
 wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/stl/main/stl/inject.py" -O /root/akun/inject.py
-opkg install openvpn-openssl && opkg install ip-full && opkg install openssh-client && opkg install stunnel && opkg install --force-depends *.ipk && opkg install lsof && opkg install fping screen python python3
+opkg install openvpn-openssl && opkg install ip-full && opkg install openssh-client && opkg install stunnel && opkg install --force-depends *.ipk && opkg install lsof && opkg install fping screen python python3 coreutils-nohup
 chmod +x /usr/bin/gproxy
 chmod +x /usr/bin/stl
 chmod +x /root/akun/tunnel.py
@@ -53,6 +53,7 @@ mkdir -p ~/.ssh/
 touch ~/akun/ssl.conf
 touch ~/.ssh/config
 touch ~/akun/stl.txt
+touch ~/akun/ipmodem.txt 
 sleep 2
 echo "install selesai"
 echo "untuk memulai tools silahkan jalankan perintah 'stl'"
