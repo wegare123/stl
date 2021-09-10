f0b1b6961c2d40c5779318368a164051c3220d8d
wegare123 <56117745+wegare123@users.noreply.github.com>
2021-09-10 12:31:02 +0700

    Add files via upload

--- stl/autorekonek-stl.sh
+++ stl/autorekonek-stl.sh
@@ -4,7 +4,7 @@ route="$(route | grep -i 8.8.8.8 | head -n1 | awk '{print $2}')"
 route2="$(route | grep -i 10.0.0.2 | head -n1 | awk '{print $2}')" 
 route3="$(lsof -i | grep -i ssh | grep -i 1080 | grep -i listen)" 
 route4="$(netstat -plantu | grep corkscrew)" 
-var=`cat /root/logs.txt | grep "Disconnected"|awk '{print $3}'`
+var=`cat /root/logs.txt | grep "Disconnected"|awk '{print $3}' 2>/dev/null`
 
 echo $route
 	if [[ -z $route2 ]]; then
