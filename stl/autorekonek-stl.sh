8b0d59e8567efc50bd69e88307ba97fb0046e572
Wega Regianto <56117745+wegare123@users.noreply.github.com>
2021-09-30 09:40:23 +0700

    Add files via upload

--- stl/autorekonek-stl.sh
+++ stl/autorekonek-stl.sh
@@ -2,8 +2,9 @@
 #stl (Wegare)
 route="$(route | grep -i 8.8.8.8 | head -n1 | awk '{print $2}')" 
 route2="$(route | grep -i 10.0.0.2 | head -n1 | awk '{print $2}')" 
-route3="$(lsof -i | grep -i ssh | grep -i 1080 | grep -i listen)" 
+route3="$(netstat -plantu | grep -i ssh | grep -i 1080 | grep -i listen)" 
 route4="$(netstat -plantu | grep corkscrew)" 
+route5="$(netstat -plantu | grep ssh | grep CLOSE_WAIT | awk '{print $6}' | tail -n1)" 
 var="$(cat /root/logs.txt 2>/dev/null)"
 
 echo $route
@@ -17,6 +18,9 @@ echo $route
            printf '\n' | stl
            exit
     elif [[ -z $route4 ]]; then
+           printf '\n' | stl
+           exit
+    elif [[ $route5 = 'CLOSE_WAIT' ]]; then
            printf '\n' | stl
            exit
     else 
