d3650a7bab34da8ebc439b390b9cf8a1000fc8af
Wega Regianto <56117745+wegare123@users.noreply.github.com>
2021-10-01 21:01:38 +0700

    Update autorekonek-stl.sh

--- stl/autorekonek-stl.sh
+++ stl/autorekonek-stl.sh
@@ -4,7 +4,7 @@ route="$(route | grep -i 8.8.8.8 | head -n1 | awk '{print $2}')"
 route2="$(route | grep -i 10.0.0.2 | head -n1 | awk '{print $2}')" 
 route3="$(netstat -plantu | grep -i ssh | grep -i 1080 | grep -i listen)" 
 route4="$(netstat -plantu | grep corkscrew)" 
-route5="$(netstat -plantu | grep ssh | grep CLOSE_WAIT | awk '{print $6}' | tail -n1)" 
+route5="$(netstat -plantu | grep ssh | grep CLOSE_WAIT | awk '{print $6}' | wc -l)" 
 var="$(cat /root/logs.txt 2>/dev/null)"
 
 echo $route
@@ -20,7 +20,7 @@ echo $route
     elif [[ -z $route4 ]]; then
            printf '\n' | stl
            exit
-    elif [[ $route5 = 'CLOSE_WAIT' ]]; then
+    elif [[ $route5 -gt '5' ]]; then
            printf '\n' | stl
            exit
     else 
